85e0b632c01d0a4054e0aca9b92f61c2c5dc6dfa
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2024-09-13 20:49:36 +0200

    v2

---
+++ oppgave1/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react';
+import Counter from './components/Counter';
+import SchoolSystem from './components/SchoolSystem';
+  
+function App() {
+ 
+  
+  return (
+  <>
+  <h2>Dette er oppgave 2 :)</h2>
+  <Counter/>
+  <h2>Dette er oppgave 3 :)</h2>
+  <SchoolSystem/>
+ 
+  </>
+
+  );
+}
+
+export default App
